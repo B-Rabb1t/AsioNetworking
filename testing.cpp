e3615fd98a2fffddf68be9d5897560b28d94f8a0
B-Rabb1t <georgeborsa@gmail.com>
2022-08-31 23:22:17 +0000

    Using a TearDown

--- testing.cpp
+++ testing.cpp
@@ -6,17 +6,6 @@ using namespace std::chrono_literals;
 class CustomClient : public last::net::client_interface<CustomMsgTypes>
 {
 public:
-    void PingServer()
-    {
-        last::net::message<CustomMsgTypes> msg;
-        msg.header.id = CustomMsgTypes::ServerPing;
-
-        std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
-
-        msg << timeNow;
-        Send(msg);
-    }
-
     void MessageAll()
     {
         last::net::message<CustomMsgTypes> msg;
@@ -39,10 +28,16 @@ class ServerTest : public ::testing::Test
 
         ASSERT_TRUE(server->Start());
         running = std::thread([this](){
-            while(bQuit)
-                server->Update(-1, true);
+            while(!bQuit)
+                server->Update();
         });
-        running.detach();
+    }
+
+    void TearDown() override
+    {
+        bQuit = true;
+        running.join();
+        delete server;
     }
  };
 
@@ -52,16 +47,16 @@ TEST_F(ServerTest, Connection)
 
     c1.Connect("127.0.0.1", 60000);
     c2.Connect("127.0.0.1", 60000);
+    std::this_thread::sleep_for(10ms);
     ASSERT_EQ(2, server->GetConnectionsCount()) << "Missing connections!\n";
 
     c3.Connect("127.0.0.0", 60000);
+    std::this_thread::sleep_for(10ms);
     ASSERT_EQ(2, server->GetConnectionsCount()) << "Wrong client connected!\n";
 
     ASSERT_TRUE(c1.IsConnected()) << "Client lost!\n";
     c1.Disconnect();
     ASSERT_FALSE(c1.IsConnected()) << "Client couldn't disconnect!\n";
-
-    delete server;
 }
 
 TEST_F(ServerTest, Communication)
@@ -74,12 +69,12 @@ TEST_F(ServerTest, Communication)
     std::this_thread::sleep_for(10ms);
 
     ASSERT_FALSE(client.Incoming().empty()) << "No message received!\n";
-    EXPECT_EQ(1, client.Incoming().count()) << "Missing connection confirmation!\n";
+    ASSERT_EQ(1, client.Incoming().count()) << "Missing connection confirmation!\n";
 
     last::net::message<CustomMsgTypes> msg;
     msg.header.id = CustomMsgTypes::ServerMessage;
     server->MessageAllClients(msg);
     std::this_thread::sleep_for(10ms);
 
-    EXPECT_EQ(2, client.Incoming().count()) << "MessageAllClients Failed!\n";
+    ASSERT_EQ(2, client.Incoming().count()) << "MessageAllClients Failed!\n";
 }
